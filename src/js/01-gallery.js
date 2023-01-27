155d536426bc2da6a6da5430da34fd21690e199f
SerhiiRodin <rodinserj@ukr.net>
2023-01-27 19:05:53 +0200

    add file

---
+++ src/js/01-gallery.js
@@ -0,0 +1,28 @@
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+console.log('object dcss');
+
+// const galleryEl = document.querySelector('.gallery');
+
+// const ListItemsMarkup = createListItemsMarkup(galleryItems);
+
+// function createListItemsMarkup(items) {
+//   return items
+//     .map(item => {
+//       return `
+// <li>
+//     <a class="gallery__item" href="${item.original}">
+//         <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+//     </a>
+// </li>`;
+//     })
+//     .join('');
+// }
+
+// galleryEl.innerHTML = ListItemsMarkup;
