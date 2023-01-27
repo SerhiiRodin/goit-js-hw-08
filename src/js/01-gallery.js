6a2e093f6d14d1e50fe860fd3fb14980f3c12c17
SerhiiRodin <rodinserj@ukr.net>
2023-01-27 19:26:02 +0200

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -8,21 +8,40 @@ console.log(galleryItems);
 
 console.log('object dcss');
 
-// const galleryEl = document.querySelector('.gallery');
+const galleryEl = document.querySelector('.gallery');
 
-// const ListItemsMarkup = createListItemsMarkup(galleryItems);
+const ListItemsMarkup = createListItemsMarkup(galleryItems);
 
-// function createListItemsMarkup(items) {
-//   return items
-//     .map(item => {
-//       return `
-// <li>
-//     <a class="gallery__item" href="${item.original}">
-//         <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
-//     </a>
-// </li>`;
-//     })
-//     .join('');
-// }
+function createListItemsMarkup(items) {
+  return items
+    .map(item => {
+      return `
+<li>
+    <a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+</li>`;
+    })
+    .join('');
+}
 
-// galleryEl.innerHTML = ListItemsMarkup;
+galleryEl.innerHTML = ListItemsMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  nav: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+galleryEl.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  lightbox.open(event.target);
+}
