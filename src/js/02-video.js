d27b3f005fa29d2dbae2ff66abe7f4cd071fe752
SerhiiRodin <rodinserj@ukr.net>
2023-01-29 14:32:27 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
 
+import _ from 'lodash';
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+// player.setCurrentTime(
+//   localStorage.getItem('videoplayer-current-time')
+//     ? localStorage.getItem('videoplayer-current-time')
+//     : 0
+// );
+
+player.on('timeupdate', _.throttle(onCurrentTime, 1000));
+
+function onCurrentTime(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+// console.log(localStorage.getItem('videoplayer-current-time'));
