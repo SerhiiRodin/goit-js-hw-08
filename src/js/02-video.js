bd82f7f2f19cab3ad9ccacf82ea8ae92c04678eb
SerhiiRodin <rodinserj@ukr.net>
2023-01-30 22:28:09 +0200

    add correct

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,13 +6,13 @@ const iframe = document.querySelector('iframe');
 
 const player = new Player(iframe);
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-
-// player.setCurrentTime(
-//   localStorage.getItem('videoplayer-current-time')
-//     ? localStorage.getItem('videoplayer-current-time')
-//     : 0
-// );
+// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+// если в хранилище нет значения, то записываем 0
+player.setCurrentTime(
+  localStorage.getItem('videoplayer-current-time')
+    ? localStorage.getItem('videoplayer-current-time')
+    : 0
+);
 
 player.on('timeupdate', _.throttle(onCurrentTime, 1000));
 
